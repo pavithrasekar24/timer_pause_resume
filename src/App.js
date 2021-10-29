cb92b81e12dbd0da767db4d97a18e272197c88ac
pavithrasekar24 <spavithracse8@gmail.com>
2021-10-29 20:06:46 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -44,27 +44,3 @@ export default class App extends React.Component {
     );
   }
 }
-// var Timer = function(callback, delay) {
-//   var timerId, start, remaining = delay;
-
-//   this.pause = function() {
-//       window.clearTimeout(timerId);
-//       remaining -= Date.now() - start;
-//   };
-
-//   this.resume = function() {
-//       start = Date.now();
-//       window.clearTimeout(timerId);
-//       timerId = window.setTimeout(callback, remaining);
-//   };
-
-//   this.resume();
-// };
-
-// var timer = new Timer(function() {
-//   alert("Done!");
-// }, 1000);
-
-// timer.pause();
-// // Do some stuff...
-// timer.resume()
